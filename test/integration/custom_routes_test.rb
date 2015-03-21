d790530f8b3b070cbbd55676e35de3ba192e6310
Ben Dowling <ben.developer@icloud.com>
2015-03-21 17:00:10 +0000

    Updated profile and added scoped status to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	get '/register'
   	assert_response :success
   end
+
+  test "that a profile page works" do
+    get '/bendowling'
+    assert_response :success
+  end
 end
